84e369078e60b27a9e06f0200cf681e27a9be7cb
mishag1122@gmail.com <mishag1122@gmail.com>
2026-04-18 11:58:18 -0400

    first change

--- API/Program.cs
+++ API/Program.cs
@@ -104,5 +104,5 @@ catch (Exception ex)
     var logger = services.GetRequiredService<ILogger<Program>>();
     logger.LogError(ex, "An error occurred during migration");
 }
-
+//test comments
 app.Run();
